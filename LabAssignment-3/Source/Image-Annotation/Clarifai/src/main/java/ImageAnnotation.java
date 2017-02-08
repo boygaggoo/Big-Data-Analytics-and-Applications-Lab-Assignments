75d367ae55a113dc3586cfeb8a90fa65c8f8a2ea
meghanagabhushan <nagabhushan.megha@gmail.com>
2017-02-08 17:28:07 -0600

    code for video annotation
    code for video annotation

---
+++ LabAssignment-3/Source/Image-Annotation/Clarifai/src/main/java/ImageAnnotation.java
@@ -0,0 +1,61 @@
+import clarifai2.api.ClarifaiBuilder;
+import clarifai2.api.ClarifaiClient;
+import clarifai2.api.ClarifaiResponse;
+import clarifai2.dto.input.ClarifaiInput;
+import clarifai2.dto.input.image.ClarifaiImage;
+import clarifai2.dto.model.output.ClarifaiOutput;
+import clarifai2.dto.prediction.Concept;
+import okhttp3.OkHttpClient;
+import org.openimaj.image.DisplayUtilities;
+import org.openimaj.image.ImageUtilities;
+import org.openimaj.image.MBFImage;
+import org.openimaj.image.colour.RGBColour;
+import org.openimaj.image.typography.hershey.HersheyFont;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.List;
+
+/**
+ * Created by Naga on 24-01-2017.
+ */
+public class ImageAnnotation {
+    public static void main(String[] args) throws IOException {
+        final ClarifaiClient client = new ClarifaiBuilder("KKQIegBW9uOl_3vaMSzqq4QCfPNyNBvB7XNBz1vE", "xsY48eiDhhsFo5M7HE3F71ZYkB_tEQmemlWekTgG")
+                .client(new OkHttpClient()) // OPTIONAL. Allows customization of OkHttp by the user
+                .buildSync(); // or use .build() to get a Future<ClarifaiClient>
+        client.getToken();
+
+        File file = new File("output/mainframes");
+        File[] files = file.listFiles();
+        for (int i=0; i<files.length;i++){
+            ClarifaiResponse response = client.getDefaultModels().generalModel().predict()
+                    .withInputs(
+                            ClarifaiInput.forImage(ClarifaiImage.of(files[i]))
+                    )
+                    .executeSync();
+            List<ClarifaiOutput<Concept>> predictions = (List<ClarifaiOutput<Concept>>) response.get();
+           // PrintWriter out = new PrintWriter("example.out");
+            //for (Concept line : predictions) {
+              //  out.println(line);
+            //}
+            MBFImage image = ImageUtilities.readMBF(files[i]);
+            int x = image.getWidth();
+            int y = image.getHeight();
+
+            System.out.println("*************" + files[i] + "***********");
+            List<Concept> data = predictions.get(0).data();
+            PrintWriter writer = new PrintWriter("the-file-name.txt1", "UTF-8");
+            for (int j = 0; j < data.size(); j++) {
+                System.out.println(data.get(j).name() + " - " + data.get(j).value());
+                //writeLines(File outputFile,outputData);
+                writer.println(data.get(j).name());
+                image.drawText(data.get(j).name(), (int)Math.floor(Math.random()*x), (int) Math.floor(Math.random()*y), HersheyFont.ASTROLOGY, 20, RGBColour.RED);
+            }
+            writer.close();
+            DisplayUtilities.displayName(image, "image" + i);
+        }
+
+    }
+}
